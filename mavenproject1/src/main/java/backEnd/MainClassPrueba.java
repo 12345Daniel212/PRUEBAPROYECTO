7e9419dd1cf06a468aaf61f62251ac0c85649a94
rodri <rodri@192.168.56.1>
2025-04-20 14:35:40 -0700

    prueba desde visual

--- mavenproject1/src/main/java/backEnd/MainClassPrueba.java
+++ mavenproject1/src/main/java/backEnd/MainClassPrueba.java
@@ -1,24 +1,23 @@
 package backEnd;
-// import backEnd.Empleado;
+// import backEnd.Empleado; loca
 
 import java.util.Date;
-
 
 public class MainClassPrueba {
     public static void main(String[] args) {
 
         System.out.println("--------------------------------------------------");
 
-
-        Empleado empleado = new Empleado( 1, "9:00 AM - 5:00 PM","foto.jpg","123456789", "RFC123456","Daniel Rodriguez", "5551234567","Direccion generica 123");
+        Empleado empleado = new Empleado(1, "9:00 AM - 5:00 PM", "foto.jpg", "123456789", "RFC123456",
+                "Daniel Rodriguez", "5551234567", "Direccion generica 123");
         empleado.mostrarDatosPersona();
 
         System.out.println("--------------------------------------------------");
 
-         Cliente cliente = new Cliente( 1000.0, 5000.0 ,"ROADJADK23J2K", "DANIEL", "31110203", "Direccion generica 321");
-         cliente.mostrarDatosPersona();
+        Cliente cliente = new Cliente(1000.0, 5000.0, "ROADJADK23J2K", "DANIEL", "31110203", "Direccion generica 321");
+        cliente.mostrarDatosPersona();
 
-         System.out.println("--------------------------------------------------");
+        System.out.println("--------------------------------------------------");
 
         Marca marca = new Marca("Marca ");
         System.out.println("Marca: " + marca.getNombreMarca());
@@ -32,8 +31,9 @@ public class MainClassPrueba {
 
         System.out.println("--------------------------------------------------");
 
-        Factura factura = new Factura(001, "Factura", new Date(), 'P', 1230.0, 16.0, new Date(), "10:00", 1, "ArticuloX", 50.0, marca, tipoProducto);
-        factura.calcularTotal(); // Calcular el total de la factura
+        Factura factura = new Factura(001, "Factura", new Date(), 'P', 1230.0, 16.0, new Date(), "10:00", 1,
+                "ArticuloX", 50.0, marca, tipoProducto);
+        factura.calcularTotal();
         factura.mostrarDatosArticulo();
     }
 }
